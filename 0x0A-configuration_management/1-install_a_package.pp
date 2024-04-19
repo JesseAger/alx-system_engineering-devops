c0c27374d84fd28c5f01c2882cbaf9cf105e72fb
JesseAger <agerjesse6@gmail.com>
2024-04-19 15:07:50 -0400

    test_pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-# install puppet-lint -v 2.5.0
+# install flask version 2.1.0
 
-package { 'pip3':
-  ensure => installed,
-}
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['pip3'],
+exec { 'puppet-lint':
+  command => '/usr/bin/pip3 install flask -v 2.1.0',
 }
