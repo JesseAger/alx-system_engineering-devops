e53dbd3373c1a2c553cfc4df092c4ef044ae15de
JesseAger <agerjesse6@gmail.com>
2024-04-19 15:02:59 -0400

    flask 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install puppet-lint -v 2.5.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get pip3 install flask -v 2.1.0',
+package { 'pip3':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['pip3'],
 }
